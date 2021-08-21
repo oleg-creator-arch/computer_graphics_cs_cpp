8517430f8ff222dfba96d0f5d976b9820d9cc14d
oleg-creator-arch <60305433+oleg-creator-arch@users.noreply.github.com>
2021-08-21 18:09:37 +0300

    Add files via upload

---
+++ fill pixel by pixel/laba3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Purple;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Алгоритм 4-связной гранично-определенной области";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(445, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Интервальный затравочный алгоритм";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 416);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(446, 134);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(249, 264);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(539, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Логи";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная 3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ fill pixel by pixel/laba3/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace laba3
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch myStopwatch = new System.Diagnostics.Stopwatch();
+        Graphics g;
+        Bitmap buf;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Алгоритм 4-связной гранично-определенной области
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myStopwatch.Restart();
+            myStopwatch.Start();
+            // Очистка холста
+            g.Clear(Color.Black);
+            // Очистка ListBox1
+            listBox1.Items.Clear();
+
+            // Рисуем область
+            buf = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(buf);
+            g.DrawRectangle(new Pen(Color.Red, 1), 50, 50, 100, 100);
+            buf.SetPixel(100, 100, Color.Red);
+            for (int i = 0; i < 30; i++) buf.SetPixel(100 - i, 100 + i, Color.Red);
+            for (int i = 0; i < 30; i++) buf.SetPixel(100 + i, 100 + i, Color.Red);
+            for (int i = 0; i < 30; i++) buf.SetPixel(100 - i, 100 + i, Color.Red);
+            for (int i = 0; i < 10; i++) buf.SetPixel(100 - 30 + i, 100 + 30, Color.Red);
+            for (int i = 0; i < 10; i++) buf.SetPixel(100 + 30 - i, 100 + 30, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 - 20 - i, 100 + 30 + i, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 + 20 + i, 100 + 30 + i, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 - 40 + i, 100 + 50, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 + 40 - i, 100 + 50, Color.Red);
+
+            //координаты точки
+            int x;
+            int y;
+            pix x_y;
+
+            Color oldcolor = Color.FromArgb(255, 255, 255, 255); // исходное значение цвета черный
+            listBox1.Items.Add("Цвет oldcolor - " + @oldcolor);
+
+            Color newcolor = Color.FromArgb(255, 0, 255, 100); // новое значение цвета зеленый 
+            listBox1.Items.Add("Цвет newcolor - " + @newcolor);
+
+            Color framecolor = Color.FromArgb(255, 255, 0, 0);// цвет границы красный 
+            listBox1.Items.Add("Цвет framecolor - " + @framecolor);
+
+            Stack<pix> stack = new Stack<pix>(); // создаем стек для пикселей
+
+            // помещаем затравочный пиксель в стек
+            stack.Push(new pix(55, 55));
+            listBox1.Items.Add("-------------------------------- ");
+
+            // помещаем пиксель в стек
+            while (stack.Count != 0) // пока стек не пуст
+            {
+                // извлекаем пиксель из стека
+                x_y = stack.Pop();
+                x = x_y.getX();
+                y = x_y.getY();
+
+                // рисуем пиксель
+                PutPixel(g, newcolor, x, y);
+
+                // для точек, соседних с (x,y): 
+                // (x+1,y); (x,y+1); (x-1,y); (x,y-1). Проверяем условие
+                // (x+1,y)
+                if (buf.GetPixel(x + 1, y) != newcolor && buf.GetPixel(x + 1, y) != framecolor)
+                {
+                    // рисуем пиксель
+                    PutPixel(g, newcolor, x + 1, y);
+                    stack.Push(new pix(x + 1, y));
+                }
+                // (x,y+1)
+                if (buf.GetPixel(x, y + 1) != newcolor && buf.GetPixel(x, y + 1) != framecolor)
+                {
+                    // рисуем пиксель
+                    PutPixel(g, newcolor, x, y + 1);
+                    stack.Push(new pix(x, y + 1));
+                }
+                // (x-1, y)
+                if (buf.GetPixel(x - 1, y) != newcolor && buf.GetPixel(x - 1, y) != framecolor)
+                {
+                    // рисуем пиксель
+                    PutPixel(g, newcolor, x - 1, y);
+                    stack.Push(new pix(x - 1, y));
+                }
+                //(x, y - 1)
+                if (buf.GetPixel(x, y - 1) != newcolor && buf.GetPixel(x, y - 1) != framecolor)
+                {
+                    // рисуем пиксель
+                    PutPixel(g, newcolor, x, y - 1);
+                    stack.Push(new pix(x, y - 1));
+                }
+                pictureBox1.Image = buf;
+                pictureBox1.Refresh();
+            }
+            myStopwatch.Stop();
+            listBox1.Items.Add("Конец работы алгоритма 4-связной");
+            listBox1.Items.Add("гранично-определенной области");
+            listBox1.Items.Add("время работы 4-связного алгоритма:");
+            listBox1.Items.Add(myStopwatch.ElapsedMilliseconds + " мс");
+        }
+
+        // Интервальный затравочный алгоритм
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myStopwatch.Restart();
+            myStopwatch.Start();
+            // Очистка холста
+            g.Clear(Color.Black);
+            // Очистка ListBox1
+            listBox1.Items.Clear();
+
+            // Рисуем область
+            buf = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(buf);
+            g.DrawRectangle(new Pen(Color.Red, 1), 50, 50, 100, 100);
+            buf.SetPixel(100, 100, Color.Red);
+            for (int i = 0; i < 30; i++) buf.SetPixel(100 - i, 100 + i, Color.Red);
+            for (int i = 0; i < 30; i++) buf.SetPixel(100 + i, 100 + i, Color.Red);
+            for (int i = 0; i < 30; i++) buf.SetPixel(100 - i, 100 + i, Color.Red);
+            for (int i = 0; i < 10; i++) buf.SetPixel(100 - 30 + i, 100 + 30, Color.Red);
+            for (int i = 0; i < 10; i++) buf.SetPixel(100 + 30 - i, 100 + 30, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 - 20 - i, 100 + 30 + i, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 + 20 + i, 100 + 30 + i, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 - 40 + i, 100 + 50, Color.Red);
+            for (int i = 0; i < 20; i++) buf.SetPixel(100 + 40 - i, 100 + 50, Color.Red);
+            
+
+            //координаты точки
+            int x;
+            int y;
+            pix x_y;
+            int xl; // крайняя левая точка интервала
+            int xr; // крайняя правая точка интервала
+            int xt; // буфер x
+            int F = 0; // флаг
+
+            Color oldcolor = Color.FromArgb(255, 255, 255, 255); // исходное значение цвета черный
+            listBox1.Items.Add("Цвет oldcolor - " + @oldcolor);
+
+            Color newcolor = Color.FromArgb(255, 0, 255, 100); // новое значение цвета зеленый 
+            listBox1.Items.Add("Цвет newcolor - " + @newcolor);
+
+            Color framecolor = Color.FromArgb(255, 255, 0, 0);// цвет границы красный 
+            listBox1.Items.Add("Цвет framecolor - " + @framecolor);
+
+            Stack<pix> stack = new Stack<pix>(); // создаем стек для пикселей
+
+            // помещаем затравочный пиксель в стек
+            stack.Push(new pix(55, 55));
+            listBox1.Items.Add("-------------------------------- ");
+
+            // помещаем пиксель в стек
+            while (stack.Count != 0) // пока стек не пуст
+            {
+                // извлекаем пиксель из стека,
+                // закрашиваем и заполняем интервалы
+                // слева и справа с запоминанием крайних точек
+                x_y = stack.Pop();
+                x = x_y.getX();
+                y = x_y.getY();
+
+                //запоминаем x
+                xt = x;
+
+                // рисуем пиксель
+                PutPixel(g, newcolor, x, y);
+
+                // Заполняем интервал справа от затравки
+                x++;
+                while (buf.GetPixel(x, y) != framecolor)
+                {
+                    PutPixel(g, newcolor, x, y);
+                    x++;
+                }
+
+                //запоминаем крайний правый пиксел
+                xr = x - 1;
+
+                // восстанавливаем x
+                x = xt;
+
+                // заполняем интервал слева от затравки
+                x--;
+                while (buf.GetPixel(x, y) != framecolor)
+                {
+                    PutPixel(g, newcolor, x, y);
+                    x--;
+                }
+
+                // запоминаем крайний левый пиксель
+                xl = x + 1;
+
+                // обработка строки y = y + 1 
+                // с определением затравочной точки
+
+                y++;
+                x = xl;
+                while (x < xr)
+                {
+                    F = 0; // флаг
+                    while (buf.GetPixel(x, y) != framecolor &&
+                        buf.GetPixel(x, y) != newcolor &&
+                        x < xr)
+                    {
+                        if (F == 0)
+                        {
+                            F = 1;
+                        }
+                        x++;
+                    }
+                    if (F == 1)
+                    {
+                        if (x == xr && buf.GetPixel(x, y) != framecolor &&
+                            buf.GetPixel(x, y) != newcolor)
+                        {
+                            stack.Push(new pix(x, y));
+                        }
+                        else
+                        {
+                            stack.Push(new pix(x - 1, y));
+                        }
+                        F = 0;
+                    }
+                    xt = x;
+                    while (buf.GetPixel(x, y) == framecolor ||
+                        buf.GetPixel(x, y) == newcolor && x < xr)
+                    {
+                        x++;
+                    }
+                    if (x == xt)
+                    {
+                        x++;
+                    }
+                }
+
+                //обработка строки y = y - 1
+                y = y - 2;
+                x = xl;
+                while (x < xr)
+                {
+                    F = 0; // флаг
+                    while (buf.GetPixel(x, y) != framecolor &&
+                        buf.GetPixel(x, y) != newcolor &&
+                        x < xr)
+                    {
+                        if (F == 0)
+                        {
+                            F = 1;
+                        }
+                        x++;
+                    }
+                    if (F == 1)
+                    {
+                        if (x == xr && buf.GetPixel(x, y) != framecolor &&
+                            buf.GetPixel(x, y) != newcolor)
+                        {
+                            stack.Push(new pix(x, y));
+                        }
+                        else
+                        {
+                            stack.Push(new pix(x - 1, y));
+                        }
+                        F = 0;
+                    }
+                    xt = x;
+                    while (buf.GetPixel(x, y) == framecolor ||
+                        buf.GetPixel(x, y) == newcolor && x < xr)
+                    {
+                        x++;
+                    }
+                    if (x == xt)
+                    {
+                        x++;
+                    }
+                }
+                pictureBox1.Image = buf;
+                pictureBox1.Refresh();
+            }
+            myStopwatch.Stop();
+            listBox1.Items.Add("Конец работы интервального");
+            listBox1.Items.Add("затравочного алгоритма");
+            listBox1.Items.Add("время работы");
+            listBox1.Items.Add("интервального затравочного алгоритма:");
+            listBox1.Items.Add(myStopwatch.ElapsedMilliseconds + " мс");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = this.pictureBox1.CreateGraphics(); // создаем объект - холст
+            g.Clear(Color.Black);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Метод, устанавливающий пиксел на форме с заданными цветом
+        private static void PutPixel(Graphics g, Color col, int x, int y)
+        {
+            g.FillRectangle(new SolidBrush(col), x, y, 1, 1);
+        }
+
+        public class pix : Form1 // класс пиксель
+        {
+            // координаты пикселя
+            int x;
+            int y;
+            public pix(int x1, int y1)
+            {
+                x = x1;
+                y = y1;
+            }
+            public int getX()
+            {
+                return x;
+            }
+            public int getY()
+            {
+                return y;
+            }
+        }
+    }
+}
