884242263ac9a899e91c2cd237ad2a8203af4288
oleg-creator-arch <60305433+oleg-creator-arch@users.noreply.github.com>
2021-08-21 18:06:03 +0300

    Add files via upload

---
+++ splash screen/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace OlegxasanovKG1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 222);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 173);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(15, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(292, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "бонус";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(313, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(727, 386);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Lime;
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 85);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "стиль стирания";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(213, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Стирание изображения цветом фона";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(173, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Вывод спрайта изображения";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Lime;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(106, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Очистка экрана";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(15, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(292, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "старт";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(15, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(292, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1052, 407);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Олег Хасанов ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ splash screen/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OlegxasanovKG1
+{
+    public partial class Form1 : Form
+    {
+        //Бонус
+        int i; //Переменная используемая в цикле, обозначает кол-во проходов
+        double l, a; //Переменные используемые для построения каждого квадрата
+        const
+        int d0 = 115, x0 = 105, y0 = 90; //Константа, влияющая на расстояние от
+                                         //центра квадрата до углов(Не диагональ),
+                                         //подобрана экспериментальным путем*/
+                                         //Координаты центра квадрата
+
+        /*  Recx0 = 10,
+          Recy0 = 15,
+          Recx00 = 75,
+          Recy00 = 75,
+          Linx0 = 10, 
+          Liny0 = 15,
+          Linx00 = 85, 
+          Liny00 = 90;*/
+        int Recx0 = 10,
+        Recy0 = 15,
+        Recx00 = 75,
+        Recy00 = 75,
+        Linx0 = 10,
+        Liny0 = 15,
+        Linx00 = 85,
+        Liny00 = 90;
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "старт")
+            {
+                button2.Text = "стоп";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                button2.Text = "старт";
+                timer1.Enabled = false;
+            }
+
+
+
+
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            drawfigyra();
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //смещение по углу 
+        int vecX = -(int)(Math.Cos((165 / 180D) * Math.PI) * 10);
+        int vecY = (int)(Math.Sin((165 / 180D) * Math.PI) * 10);
+
+        void perehod()
+        {
+            //переход квадрата и линий
+            Recx0 += vecX;
+            Linx0 += vecX;
+            Linx00 += vecX;
+            if (Recx0 + vecX + Recx00 >= panel1.Width || Recx0 + vecX <= 0)
+            {
+                vecX *= -1;
+            }
+
+            Recy0 += vecY;
+            Liny0 += vecY;
+            Liny00 += vecY;
+            if (Recy0 + vecY + Recy00 >= panel1.Height || Recy0 + vecY <= 0)
+            {
+                vecY *= -1;
+            }
+
+        }
+
+
+
+        void drawfigyra()
+        {
+            //рандомный цвет
+            Random BcBet = new Random();
+            Pen rychka = new Pen(Color.Red, 3);
+            SolidBrush zalivka = new SolidBrush(Color.Black);
+            if (BcBet.Next(3) == 1)
+            {
+                rychka.Color = Color.Green;
+
+            }
+            else
+            if (BcBet.Next(3) == 2)
+            {
+                rychka.Color = Color.Red;
+
+            }
+            else
+            if (BcBet.Next(3) == 2)
+            {
+                rychka.Color = Color.Blue;
+
+            }
+
+            Graphics oblast = panel1.CreateGraphics();
+            oblast.DrawRectangle(rychka, Recx0, Recy0, Recx00, Recy00);
+            oblast.DrawLine(rychka, Linx0, Liny0, Linx00, Liny00);
+            oblast.DrawLine(rychka, Linx00, Liny0, Linx0, Liny00);
+            System.Threading.Thread.Sleep(300);
+
+            //стирание 
+            //Очистка экрана
+            if (radioButton1.Checked == true)
+            {
+                
+                panel1.Invalidate();
+                panel1.Update();
+
+            }
+            //Вывод спрайта изображения
+            if (radioButton2.Checked == true)
+            {
+                //координаты скоректированы из-за толщины пера
+                oblast.FillRectangle(zalivka, Recx0 - 5, Recy0 - 5, Recx00 + 10, Recy00 + 10);
+            }
+            //Стирание изображения цветом фона.
+            if (radioButton3.Checked == true)
+            {
+                oblast.Clear(Color.Black);
+
+
+            }
+            perehod();
+
+
+
+
+
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        void DrawSquare(double c, double b) //функция построения и отрисовки квадрата
+        {
+            int x1, x2, x3, x4, y1, y2, y3, y4; //Координаты углов каждого квадрата
+            Pen myPen = new Pen(Color.Green, 1); //Выбираем перо "myPen" черного цвета green толщиной в 1 пиксель
+            Graphics g = pictureBox1.CreateGraphics();//Объявляем объект "g" класса Graphics и 
+                                                      //предоставляем ему возможность рисования на pictureBox1:
+            x1 = (int)(x0 + c * Math.Cos(b + 1 * Math.PI / 4));//Координаты правой нижней
+            y1 = (int)(y0 + c * Math.Sin(b + 1 * Math.PI / 4));//точки
+            x2 = (int)(x0 + c * Math.Cos(b + 3 * Math.PI / 4)); //Координаты левой нижней 
+            y2 = (int)(y0 + c * Math.Sin(b + 3 * Math.PI / 4)); //точки
+            x3 = (int)(x0 + c * Math.Cos(b + 5 * Math.PI / 4)); //Координаты левой верхней
+            y3 = (int)(y0 + c * Math.Sin(b + 5 * Math.PI / 4)); //точки 
+            x4 = (int)(x0 + c * Math.Cos(b + 7 * Math.PI / 4));//Координаты правой верхней
+            y4 = (int)(y0 + c * Math.Sin(b + 7 * Math.PI / 4)); //точки
+                                                                //Метод строящий фигуру по 4 точкам
+            g.DrawPolygon(myPen, new Point[] {
+                new Point(x1,y1),new Point(x2,y2),
+                new Point(x3,y3),new Point(x4,y4),
+            });
+
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            l = d0;
+            a = 0;
+            for (i = 1; i <= 30; i++)//Цикл, рисующий 30 квадратов
+            {
+                DrawSquare(l, a); //Вызов функции построения и отрисовки квадрата
+                a = a + Math.PI / 19;//изменяет угол поворота следующих квадратов
+                l = l * Math.Sin(Math.PI / 3);//изменяет размер следующих квадратов
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
