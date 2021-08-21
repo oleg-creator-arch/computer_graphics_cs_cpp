81208c3b273ae29e75048536739ca8dd8301d413
oleg-creator-arch <60305433+oleg-creator-arch@users.noreply.github.com>
2021-08-21 18:28:18 +0300

    Add files via upload

---
+++ Image filtering/CG_lab7/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace CG_lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.openToolStripMenuItem.Text = "Открыть";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(161, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.X)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image files|*.jpg;*.png;*.bmp";
+            this.openFileDialog1.Title = "Открыть изображение";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox2.Location = new System.Drawing.Point(507, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(498, 313);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(498, 313);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(552, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(412, 100);
+            this.panel1.TabIndex = 3;
+            this.panel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 98);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ЗА РАБОТУ!!!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 71);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1008, 319);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(427, 42);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(130, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Увелечение яркости";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(294, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(132, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Уселение перепадов";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1008, 390);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Хасанов О. А. КГ - лаба №7";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Image filtering/CG_lab7/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CG_lab7
+{
+    public partial class Form1 : Form
+    {
+        //матрица фильтра
+        private static float[][] I = new float[][]
+        {
+            new float[] { 1, 2, 3 },
+            new float[] { 4, 5, 6 },
+            new float[] { 7, 8, 9 }
+        };
+
+        private float d = (
+             Math.Abs(I[0][0] - I[1][1]) + Math.Abs(I[1][0] - I[1][1]) + Math.Abs(I[2][0] - I[1][1])
+             + Math.Abs(I[0][1] - I[1][1]) + /*Math.Abs(I[1][1] - I[1][1])*/+Math.Abs(I[2][1] - I[1][1])
+             + Math.Abs(I[0][2] - I[1][1]) + Math.Abs(I[1][2] - I[1][1]) + Math.Abs(I[2][2] - I[1][1])
+             ) / 8;
+        float[,] Ir = new float[3, 3];
+        float[,] Ig = new float[3, 3];
+        float[,] Ib = new float[3, 3];
+        private float dr;
+        private float dg;
+        private float db;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Применение фильтра
+        private Bitmap filterApply(Bitmap bmp)
+        {
+            Bitmap tmp = new Bitmap(bmp.Width, bmp.Height); //битмап, в котором формируется обработанное изображение
+
+            //Заполняем массив рикселей изображения (для увеличения скорости обработки)
+            //Это работает птому что GetPixel работает очень медленно
+            Color[][] img = new Color[bmp.Height][];
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                img[y] = new Color[bmp.Width];
+                for (int x = 0; x < bmp.Width; x++)
+                    img[y][x] = bmp.GetPixel(x, y);
+            }
+
+            //Проходим по всем пикселам и вычисляем пиксели результирующего изображения
+            for (int sroki = 1; sroki < tmp.Height - 1; sroki++)
+                for (int col = 1; col < tmp.Width - 1; col++)
+                {
+                    //переменные составляющих цветов
+                    float resR = 0, //красный
+                        resG = 0,   //зелёный
+                        resB = 0;   //синий
+
+                    if (radioButton1.Checked)
+                    {
+                        //проходим по матрице фильтра и вычисляем цвет текущего пиксела
+                        for (int i = 0; i < 3; i++)
+                            for (int j = 0; j < 3; j++)
+                                //если мы не у границы, то применяем фильтр
+                                if (col - j - 1 >= 0 && col - j - 1 < bmp.Width && sroki - i - 1 >= 0 && sroki - i - 1 < bmp.Height)
+                                {
+                                    resR += (float)img[sroki - 1][col - 1].R + d; //красная составляющая
+                                    resG += (float)img[sroki - 1][col - 1].G + d; //зелёная составляющая
+                                    resB += (float)img[sroki - 1][col - 1].B + d; //синяя составляющая
+                                }
+                    }
+                    else
+                    {
+                        //проходим по матрице фильтра и вычисляем цвет текущего пиксела
+                        for (int i = 0; i < 3; i++)
+                            for (int j = 0; j < 3; j++)
+                                //если мы не у границы, то применяем фильтр
+                                if (col - j - 1 >= 0 && col - j - 1 < bmp.Width && sroki - i - 1 >= 0 && sroki - i - 1 < bmp.Height)
+                                {
+                                    resR = (float)img[sroki - 1][col - 1].R;
+                                    Ir[i, j] = resR;
+                                    resG = (float)img[sroki - 1][col - 1].G;
+                                    Ig[i, j] = resG;
+                                    resB = (float)img[sroki - 1][col - 1].B;
+                                    Ib[i, j] = resB;
+                                }
+                        //высчитываем среднее значение и прибовляем для увелечения яркости
+                        dr = (
+                            Math.Abs(Ir[0, 0] - Ir[1, 1]) + Math.Abs(Ir[1, 0] - Ir[1, 1]) + Math.Abs(Ir[2, 0] - Ir[1, 1])
+                            + Math.Abs(Ir[0, 1] - Ir[1, 1]) + /*Math.Abs(I[1][1] - I[1][1])*/+Math.Abs(Ir[2, 1] - Ir[1, 1])
+                            + Math.Abs(Ir[0, 2] - Ir[1, 1]) + Math.Abs(Ir[1, 2] - Ir[1, 1]) + Math.Abs(Ir[2, 2] - Ir[1, 1])
+                            ) / 8;
+                        dg = (
+                            Math.Abs(Ig[0, 0] - Ig[1, 1]) + Math.Abs(Ig[1, 0] - Ig[1, 1]) + Math.Abs(Ig[2, 0] - Ig[1, 1])
+                            + Math.Abs(Ig[0, 1] - Ig[1, 1]) + /*Math.Abs(I[1][1] - I[1][1])*/+Math.Abs(Ig[2, 1] - Ig[1, 1])
+                            + Math.Abs(Ig[0, 2] - Ig[1, 1]) + Math.Abs(Ig[1, 2] - Ig[1, 1]) + Math.Abs(Ig[2, 2] - Ig[1, 1])
+                            ) / 8;
+                        db = (
+                            Math.Abs(Ib[0, 0] - Ib[1, 1]) + Math.Abs(Ib[1, 0] - Ib[1, 1]) + Math.Abs(Ib[2, 0] - Ib[1, 1])
+                            + Math.Abs(Ib[0, 1] - Ib[1, 1]) + /*Math.Abs(I[1][1] - I[1][1])*/+Math.Abs(Ib[2, 1] - Ib[1, 1])
+                            + Math.Abs(Ib[0, 2] - Ib[1, 1]) + Math.Abs(Ib[1, 2] - Ib[1, 1]) + Math.Abs(Ib[2, 2] - Ib[1, 1])
+                            ) / 8;
+                        resR += (float)img[sroki - 1][col - 1].R + dr; //красная составляющая
+                        resG += (float)img[sroki - 1][col - 1].G + dg; //зелёная составляющая
+                        resB += (float)img[sroki - 1][col - 1].B + db; //синяя составляющая
+                    }
+                    //ограничивам диапазон значений [0-255]
+                    if (resR < 0) resR = 0; if (resR > 255) resR = 255; //красная составляющая
+                    if (resG < 0) resG = 0; if (resG > 255) resG = 255; //зелёная составляющая
+                    if (resB < 0) resB = 0; if (resB > 255) resB = 255; //синяя составляющая
+                    //устанавливаем вычисленный пиксел в результирующем изображении
+                    tmp.SetPixel(col, sroki, Color.FromArgb((int)Math.Round(resR), (int)Math.Round(resG), (int)Math.Round(resB)));
+                }
+
+            //возвращаем сформированное изображение
+            return tmp;
+        }
+
+        Thread task = null; //указатель на поток обработки
+                            //Кнопка загрузки файла
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //открываем диалог выбора файла
+            openFileDialog1.ShowDialog();
+
+            //если файл не выбран, то выходим.
+            if (openFileDialog1.FileName == null || openFileDialog1.FileName == "") return;
+
+            //если обработка идёт, останавливаем её
+            if (task != null) task.Abort();
+
+            //загружаем выбранный файл
+            Bitmap bmp = (Bitmap)Image.FromFile(openFileDialog1.FileName);
+
+            //сбрасываем выбранный файл из диалогового окна
+            openFileDialog1.FileName = "";
+
+            //исходное изображение устанавливаем в левую часть окна
+            pictureBox1.Image = bmp;
+
+            //запускаем поток обработки зображения
+            task = new Thread(new ParameterizedThreadStart((bmp_in) =>
+            {
+                Bitmap bmp_res = null; //битмап для копирования исходного изображения
+                                       //запрашиваем копирование битмапа и отображение сообщеня о процессе обработки
+                panel1.Invoke(new Action(() => { panel1.Visible = true; bmp_res = new Bitmap((Bitmap)bmp_in); }));
+                Bitmap tmp = filterApply(bmp_res); //применяем фильтр
+                                                   //запрашиваем установку обработанного изображения в правую часть окна
+                pictureBox2.Invoke(new Action(() => { pictureBox2.Image = tmp; }));
+                //запрашиваем скрытие сообщения о процессе обработки
+                panel1.Invoke(new Action(() => { panel1.Visible = false; }));
+                //удаляем указатель на поток обработки
+                task = null;
+            }));
+            task.Start(bmp);
+        }
+
+        //Закрытие формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //если идёт обработка, останавливаем её
+            if (task != null && task.IsAlive) task.Abort();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
